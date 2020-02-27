3af0f26cc0e18de6ac0e365581eef627fe38461a
ToczekM <malgorzata.toczek.99@gmail.com>
2020-02-27 21:42:35 +0100

    Początek

---
+++ php/1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="../background/background.css">
+    <title>Plik php</title>
+  </head>
+  <body><h1>Plik php</h1>
+  <?php
+echo "string"
+  ?>
+  </body>
+</html>
